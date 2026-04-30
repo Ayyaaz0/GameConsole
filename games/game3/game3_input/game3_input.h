23022addf1f23f38b348741caecb56349579b1c2
Alejandro Mychalkiw Castillo <alexmychalkiwcastillo@gmail.com>
2026-04-30 09:26:17 +0100

    Sets an up_held flag to game3_input whenever the joystick faces N || NW || NE.

--- games/game3/game3_input/game3_input.h
+++ games/game3/game3_input/game3_input.h
@@ -3,14 +3,15 @@
 
 #include <stdint.h> 
 
-typedef struct { 
-    int16_t dx; 
-    uint8_t jump_pressed; 
-    uint8_t dash_pressed; 
-    int16_t dash_dx; 
-    uint8_t attack_pressed; 
-    uint8_t ability_pressed; 
-} Game3_Input; 
+typedef struct {
+    int16_t dx;
+    uint8_t jump_pressed;
+    uint8_t dash_pressed;
+    int16_t dash_dx;
+    uint8_t attack_pressed;
+    uint8_t ability_pressed;
+    uint8_t up_held;
+} Game3_Input;
 
 void Game3_Input_Read(Game3_Input *input);
 
