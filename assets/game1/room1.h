9c656a7b5909596ca2432df13a27ff69fcc49754
Ayyaaz0 <ayyaaz0105@gmail.com>
2026-05-01 22:40:53 +0100

    Add Room1 map and entity data

---
+++ assets/game1/room1.h
@@ -0,0 +1,11 @@
+#ifndef ROOM1_H
+#define ROOM1_H
+
+#include <stdint.h>
+
+#define ROOM1_WIDTH 60
+#define ROOM1_HEIGHT 30
+
+extern const uint16_t room1_data[ROOM1_WIDTH * ROOM1_HEIGHT];
+
+#endif
