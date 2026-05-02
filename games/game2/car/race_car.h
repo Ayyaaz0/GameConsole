27930f14d8c3431fb52a1238233ee4c0dbd6dbe1
AmanP-dot12 <xhfq4839@leeds.ac.uk>
2026-05-02 19:56:00 +0100

    feature/car-controls

--- games/game2/car/race_car.h
+++ games/game2/car/race_car.h
@@ -2,15 +2,20 @@
 #define RACE_CAR_H
 
 #include "../input/race_input.h"
+
 #include <stdbool.h>
 #include <stdint.h>
-
 
 typedef struct {
   float x;
   float y;
+  float prev_x;
+  float prev_y;
+
   float speed;
   float heading_deg;
+  float prev_heading_deg;
+
   uint16_t width;
   uint16_t height;
   bool active;
@@ -19,6 +24,7 @@ typedef struct {
 void RaceCar_Init(RaceCar *car);
 void RaceCar_UpdatePhysics(RaceCar *car, const RaceInput *input);
 void RaceCar_Move(RaceCar *car, float dx, float dy);
+void RaceCar_RestorePreviousPosition(RaceCar *car);
 void RaceCar_ClampToScreen(RaceCar *car, uint16_t screen_width,
                            uint16_t screen_height);
 void RaceCar_ClampToHorizontalRange(RaceCar *car, float min_x, float max_x);
--- games/game2/input/race_input.c
+++ games/game2/input/race_input.c
@@ -1,55 +1,80 @@
 #include "race_input.h"
-#include "../config/race_config.h"
+
 #include "Joystick.h"
+
+#include <stddef.h>
 
 extern Joystick_cfg_t joystick_cfg;
 extern Joystick_t joystick_data;
+
+static void RaceInput_Reset(RaceInput *input) {
+  input->throttle = 0.0f;
+  input->brake = 0.0f;
+  input->steering = 0.0f;
+  input->move_x = 0.0f;
+  input->move_y = 0.0f;
+}
+
 void RaceInput_Read(RaceInput *input) {
   if (input == NULL) {
     return;
   }
 
-  input->throttle = 0.0f;
-  input->brake = 0.0f;
-  input->steering = 0.0f;
+  RaceInput_Reset(input);
 
   Joystick_Read(&joystick_cfg, &joystick_data);
 
   switch (joystick_data.direction) {
   case N:
     input->throttle = 1.0f;
-    break;
-
-  case S:
-    input->brake = 1.0f;
-    break;
-
-  case W:
-    input->steering = -1.0f;
-    break;
-
-  case E:
-    input->steering = 1.0f;
-    break;
-
-  case NW:
-    input->throttle = 1.0f;
-    input->steering = -1.0f;
+    input->move_y = -1.0f;
     break;
 
   case NE:
     input->throttle = 1.0f;
     input->steering = 1.0f;
+    input->move_x = 1.0f;
+    input->move_y = -1.0f;
     break;
 
-  case SW:
-    input->brake = 1.0f;
-    input->steering = -1.0f;
+  case E:
+    input->throttle = 1.0f;
+    input->steering = 1.0f;
+    input->move_x = 1.0f;
     break;
 
   case SE:
+    input->throttle = 1.0f;
     input->brake = 1.0f;
     input->steering = 1.0f;
+    input->move_x = 1.0f;
+    input->move_y = 1.0f;
+    break;
+
+  case S:
+    input->brake = 1.0f;
+    input->move_y = 1.0f;
+    break;
+
+  case SW:
+    input->throttle = 1.0f;
+    input->brake = 1.0f;
+    input->steering = -1.0f;
+    input->move_x = -1.0f;
+    input->move_y = 1.0f;
+    break;
+
+  case W:
+    input->throttle = 1.0f;
+    input->steering = -1.0f;
+    input->move_x = -1.0f;
+    break;
+
+  case NW:
+    input->throttle = 1.0f;
+    input->steering = -1.0f;
+    input->move_x = -1.0f;
+    input->move_y = -1.0f;
     break;
 
   case CENTRE:
