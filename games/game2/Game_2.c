6dd1af81d8154b5923883de67b43c1801b8ec85f
AmanP-dot12 <xhfq4839@leeds.ac.uk>
2026-05-05 00:12:08 +0100

    refactor/ rendering system

--- games/game2/Game_2.c
+++ games/game2/Game_2.c
@@ -2,7 +2,9 @@
 
 #include "InputHandler.h"
 #include "LCD.h"
+#include "stm32l4xx_hal.h"
 
+#include "camera/race_camera.h"
 #include "car/race_car.h"
 #include "collisions/race_collisions.h"
 #include "config/race_config.h"
@@ -17,121 +19,119 @@ extern ST7789V2_cfg_t cfg0;
 
 static RaceCar g_player_car;
 static RaceTrack g_track;
+static RaceCamera g_camera;
 static RaceCollisionState g_collision;
 static RaceState g_race_state;
+
 static bool g_game2_should_exit = false;
 
-static void Game2_PositionCarAtCurrentTileSpawn(void) {
-  g_player_car.x = RaceTrack_GetSpawnX(&g_track);
-  g_player_car.y = RaceTrack_GetSpawnY(&g_track);
+static void Game2_PositionPlayerAtStart(void) {
+  RaceCar_Init(&g_player_car);
+
+  g_player_car.x = RACE_PLAYER_START_X;
+  g_player_car.y = RACE_PLAYER_START_Y;
+
   g_player_car.prev_x = g_player_car.x;
   g_player_car.prev_y = g_player_car.y;
-  g_player_car.heading_deg = RaceTrack_GetSpawnHeadingDeg(&g_track);
-  g_player_car.prev_heading_deg = g_player_car.heading_deg;
+
+  g_player_car.vx = 0.0f;
+  g_player_car.vy = 0.0f;
   g_player_car.speed = 0.0f;
+
+  g_player_car.heading_deg = 0.0f;
+  g_player_car.prev_heading_deg = 0.0f;
 }
 
 static void Game2_ResetRace(void) {
-  RaceTrack_ResetToStart(&g_track);
-  RaceCar_Init(&g_player_car);
-  Game2_PositionCarAtCurrentTileSpawn();
+  RaceTrack_Init(&g_track, RACE_SCREEN_WIDTH, RACE_SCREEN_HEIGHT);
+  RaceTrack_StartTimer(&g_track, HAL_GetTick());
+
+  Game2_PositionPlayerAtStart();
+
+  RaceCamera_Init(&g_camera);
+  RaceCamera_Reset(&g_camera, &g_player_car);
+
+  RaceTrack_SetCamera(&g_track, g_camera.x, g_camera.y);
 
   RaceCollision_Reset(&g_collision);
   RaceState_Init(&g_race_state);
-}
-
-static bool Game2_CarReachedCorrectExit(void) {
-  RaceTrackEdge exit_edge = RaceTrack_GetCurrentExitEdge(&g_track);
-
-  switch (exit_edge) {
-  case RACE_EDGE_TOP:
-    return g_player_car.y <= 24.0f;
-
-  case RACE_EDGE_RIGHT:
-    return (g_player_car.x + (float)g_player_car.width) >=
-           ((float)RACE_SCREEN_WIDTH - 2.0f);
-
-  case RACE_EDGE_BOTTOM:
-    return (g_player_car.y + (float)g_player_car.height) >=
-           ((float)RACE_SCREEN_HEIGHT - 2.0f);
-
-  case RACE_EDGE_LEFT:
-    return g_player_car.x <= 2.0f;
-
-  default:
-    return false;
-  }
-}
-
-static void Game2_MoveToNextTile(void) {
-  RaceTrack_GoToNextScreen(&g_track);
-  Game2_PositionCarAtCurrentTileSpawn();
-}
-
-static void Game2_CheckCrashFromCollision(float impact_speed) {
-  if ((g_collision.hit_any_edge == true) &&
-      (impact_speed >= RACE_CRASH_SPEED_THRESHOLD)) {
-    RaceState_SetCrashed(&g_race_state);
-
-    g_player_car.speed = 0.0f;
-  }
 }
 
 void game2_init(void) {
   g_game2_should_exit = false;
 
-  RaceTrack_Init(&g_track, RACE_SCREEN_WIDTH, RACE_SCREEN_HEIGHT);
-  RaceCar_Init(&g_player_car);
-  Game2_PositionCarAtCurrentTileSpawn();
+  Game2_ResetRace();
 
-  RaceCollision_Reset(&g_collision);
-  RaceState_Init(&g_race_state);
-
-  LCD_Fill_Buffer(0);
+  LCD_Fill_Buffer(0U);
   LCD_Refresh(&cfg0);
 }
 
-void game2_update(void) {
+static void Game2_UpdateRunningRace(void) {
   RaceInput input = {0};
-  float impact_speed = 0.0f;
 
+  RaceInput_Read(&input);
+
+  RaceCar_UpdatePhysics(&g_player_car, &input);
+
+  RaceCar_ClampToWorld(&g_player_car, RACE_WORLD_WIDTH_PX,
+                       RACE_WORLD_HEIGHT_PX);
+
+  RaceCollision_HandleRoadEdges(&g_collision, &g_player_car, &g_track);
+
+  RaceTrack_UpdateProgress(&g_track, g_player_car.x, g_player_car.y,
+                           g_player_car.width, g_player_car.height);
+
+  RaceTrack_UpdateTimer(&g_track, HAL_GetTick());
+
+  RaceCamera_Update(&g_camera, &g_player_car, &g_track);
+}
+
+static void Game2_UpdateFinishedRace(void) {
+  RaceInput input = {0};
+
+  RaceInput_Read(&input);
+
+  g_player_car.vx *= 0.90f;
+  g_player_car.vy *= 0.90f;
+
+  g_player_car.x += g_player_car.vx;
+  g_player_car.y += g_player_car.vy;
+
+  RaceCar_ClampToWorld(&g_player_car, RACE_WORLD_WIDTH_PX,
+                       RACE_WORLD_HEIGHT_PX);
+
+  if (input.brake > 0.0f) {
+    Game2_ResetRace();
+  }
+
+  RaceCamera_Update(&g_camera, &g_player_car, &g_track);
+}
+
+void game2_update(void) {
   if (current_input.b1_pressed) {
     g_game2_should_exit = true;
     return;
   }
 
   if (RaceState_IsCrashed(&g_race_state)) {
-    RaceInput_Read(&input);
-
-    if (input.brake > 0.0f) {
-      Game2_ResetRace();
-    }
-
+    Game2_ResetRace();
     return;
   }
 
-  RaceInput_Read(&input);
-  RaceCar_UpdatePhysics(&g_player_car, &input);
-
-  impact_speed = g_player_car.speed;
-
-  if (impact_speed < 0.0f) {
-    impact_speed = -impact_speed;
-  }
-
-  if (Game2_CarReachedCorrectExit()) {
-    Game2_MoveToNextTile();
+  if (RaceTrack_IsFinished(&g_track)) {
+    Game2_UpdateFinishedRace();
     return;
   }
 
-  RaceCollision_HandleRoadEdges(&g_collision, &g_player_car, &g_track);
-  Game2_CheckCrashFromCollision(impact_speed);
+  Game2_UpdateRunningRace();
 }
 
-void game2_render(void) { RaceRender_DrawFrame(&g_track, &g_player_car); }
+void game2_render(void) {
+  RaceRender_DrawFrame(&g_track, &g_player_car, &g_camera);
+}
 
 void game2_shutdown(void) {
-  LCD_Fill_Buffer(0);
+  LCD_Fill_Buffer(0U);
   LCD_Refresh(&cfg0);
 }
 
--- games/game2/render/race_render.h
+++ games/game2/render/race_render.h
@@ -1,9 +1,11 @@
 #ifndef RACE_RENDER_H
 #define RACE_RENDER_H
 
+#include "../camera/race_camera.h"
 #include "../car/race_car.h"
 #include "../track/race_track.h"
 
-void RaceRender_DrawFrame(const RaceTrack *track, const RaceCar *player_car);
+void RaceRender_DrawFrame(const RaceTrack *track, const RaceCar *player_car,
+                          const RaceCamera *camera);
 
 #endif
