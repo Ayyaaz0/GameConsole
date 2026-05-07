7c2ce368fdec4416e4d68573e789065948411a6e
AmanP-dot12 <xhfq4839@leeds.ac.uk>
2026-05-07 01:01:42 +0100

    /feature finalise track layout checkpoints and racing route

--- games/game2/track/race_track.h
+++ games/game2/track/race_track.h
@@ -19,10 +19,9 @@ typedef struct {
   uint8_t next_checkpoint;
   uint8_t checkpoint_count;
 
+  bool joker_lap_active;
   bool joker_lap_done;
   bool joker_lap_required;
-  bool joker_lap_active;
-
 
   uint32_t race_start_ms;
   uint32_t elapsed_ms;
@@ -30,29 +29,25 @@ typedef struct {
   bool race_finished;
   bool active;
 
-// Store tile IDs as uint8_t to save memory
-uint8_t tiles[RACE_WORLD_TILES_Y][RACE_WORLD_TILES_X];
+  // Store tile IDs as uint8_t to save SRAM
+  uint8_t tiles[RACE_WORLD_TILES_Y][RACE_WORLD_TILES_X];
 } RaceTrack;
 
 void RaceTrack_Init(RaceTrack *track, uint16_t screen_width,
                     uint16_t screen_height);
-
 void RaceTrack_ResetToStart(RaceTrack *track);
 
 RaceTileType RaceTrack_GetTileAt(const RaceTrack *track, int tile_x,
                                  int tile_y);
-
 RaceTileType RaceTrack_GetTileAtWorld(const RaceTrack *track, float world_x,
                                       float world_y);
 
 bool RaceTrack_PointIsDriveable(const RaceTrack *track, float world_x,
                                 float world_y);
-
 bool RaceTrack_CarIsDriveable(const RaceTrack *track, float car_x, float car_y,
                               uint16_t car_width, uint16_t car_height);
 
 void RaceTrack_SetCamera(RaceTrack *track, float camera_x, float camera_y);
-
 int16_t RaceTrack_WorldToScreenX(const RaceTrack *track, float world_x);
 int16_t RaceTrack_WorldToScreenY(const RaceTrack *track, float world_y);
 
@@ -66,10 +61,10 @@ uint8_t RaceTrack_GetCurrentLap(const RaceTrack *track);
 uint8_t RaceTrack_GetTotalLaps(const RaceTrack *track);
 uint8_t RaceTrack_GetNextCheckpoint(const RaceTrack *track);
 uint8_t RaceTrack_GetCheckpointCount(const RaceTrack *track);
-
 uint32_t RaceTrack_GetElapsedMs(const RaceTrack *track);
 bool RaceTrack_IsFinished(const RaceTrack *track);
 
+bool RaceTrack_IsJokerLapActive(const RaceTrack *track);
 bool RaceTrack_IsJokerLapDone(const RaceTrack *track);
 bool RaceTrack_IsJokerLapRequired(const RaceTrack *track);
 
@@ -78,35 +73,26 @@ const char *RaceTrack_GetCurrentCornerName(const RaceTrack *track);
 
 RaceTriggerZone RaceTrack_GetStartFinishZone(void);
 RaceTriggerZone RaceTrack_GetCheckpointZone(uint8_t checkpoint_number);
-RaceTriggerZone RaceTrack_GetJokerZone(void);
 RaceTriggerZone RaceTrack_GetJokerStartZone(void);
 RaceTriggerZone RaceTrack_GetJokerEndZone(void);
 
 void RaceTrack_GoToNextScreen(RaceTrack *track);
 const void *RaceTrack_GetCurrentScreen(const RaceTrack *track);
 int RaceTrack_GetCurrentScreenIndex(const RaceTrack *track);
-
 int16_t RaceTrack_GetCenterX(const RaceTrack *track);
 int16_t RaceTrack_GetLeftEdgeX(const RaceTrack *track);
 int16_t RaceTrack_GetRightEdgeX(const RaceTrack *track);
-
 int16_t RaceTrack_GetCenterXAtScreenY(const RaceTrack *track,
                                       uint16_t screen_y);
-uint16_t RaceTrack_GetWidthAtScreenY(const RaceTrack *track,
-                                     uint16_t screen_y);
+uint16_t RaceTrack_GetWidthAtScreenY(const RaceTrack *track, uint16_t screen_y);
 int16_t RaceTrack_GetLeftEdgeXAtScreenY(const RaceTrack *track,
                                         uint16_t screen_y);
 int16_t RaceTrack_GetRightEdgeXAtScreenY(const RaceTrack *track,
                                          uint16_t screen_y);
-
 void RaceTrack_GetDriveBounds(const RaceTrack *track, int16_t *min_x,
                               int16_t *max_x);
-
-bool RaceTrack_HasLeftCurbAtScreenY(const RaceTrack *track,
-                                    uint16_t screen_y);
-bool RaceTrack_HasRightCurbAtScreenY(const RaceTrack *track,
-                                     uint16_t screen_y);
-
+bool RaceTrack_HasLeftCurbAtScreenY(const RaceTrack *track, uint16_t screen_y);
+bool RaceTrack_HasRightCurbAtScreenY(const RaceTrack *track, uint16_t screen_y);
 float RaceTrack_GetNextSpawnX(const RaceTrack *track);
 
 #endif
--- games/game2/track/race_track_layout.h
+++ games/game2/track/race_track_layout.h
@@ -11,18 +11,20 @@ typedef enum {
 } RaceSector;
 
 typedef enum {
-  RACE_TILE_SAND = 0,
-  RACE_TILE_GRASS,
+  RACE_TILE_GRASS = 0,
+  RACE_TILE_GRASS_DARK,
   RACE_TILE_ROAD,
-  RACE_TILE_ROAD_EDGE,
+  RACE_TILE_ROAD_DARK,
   RACE_TILE_CURB_RED,
   RACE_TILE_CURB_WHITE,
   RACE_TILE_START,
   RACE_TILE_CHECKPOINT,
   RACE_TILE_JOKER,
+  RACE_TILE_SAND,
   RACE_TILE_TYRE,
   RACE_TILE_BARRIER,
-  RACE_TILE_BUILDING
+  RACE_TILE_BUILDING,
+  RACE_TILE_TREE
 } RaceTileType;
 
 typedef struct {
@@ -33,5 +35,6 @@ typedef struct {
 } RaceTriggerZone;
 
 bool RaceTrackLayout_IsDriveableTile(RaceTileType tile);
+bool RaceTrackLayout_IsSlowTile(RaceTileType tile);
 
 #endif
